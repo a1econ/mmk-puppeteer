7bb0dbbcc011f2a88960066ebe3be7db99562feb
a1econ <79416645+a1econ@users.noreply.github.com>
2025-07-14 23:16:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ const app = express();
 
 app.get('/json', async (req, res) => {
   console.log('🧠 Λήψη JSON μέσω Puppeteer...');
-  const browser = await puppeteer.launch({ headless: true });
+ const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu'
+  ]
+});
   const page = await browser.newPage();
 
   try {
